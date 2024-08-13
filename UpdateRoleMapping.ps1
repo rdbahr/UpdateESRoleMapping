a8eac6a0576dd95a0716f223c349e522c2cb26a9
rdbahr <48165244+rdbahr@users.noreply.github.com>
2024-08-13 09:19:06 -0600

    Update UpdateRoleMapping.ps1

--- UpdateRoleMapping.ps1
+++ UpdateRoleMapping.ps1
@@ -3,7 +3,7 @@
 
 # vars for logs
 $fileoutputdate = (Get-Date -Format yyyy-MM-dd)
-$logFile = ".\logs\provisioner-" + $actionType + "_" + $fileoutputdate + ".txt"
+$logFile = ".\logs\RoleMappingSync_" + $fileoutputdate + ".txt"
 
 function WriteLog {
     param($logMessage)
@@ -20,27 +20,27 @@ $apikey = "yourApiKey"
 $endpoint = "_security/role_mapping"
 
 $ADtoESMap = @{
-  "AD Group Name" = "es_role_mapping"
+    "AD Group Name" = "es_role_mapping"
 }
 
 WriteLog "Starting script."
 Foreach ($ADGroup in $ADtoESMap.Keys) {
-  $ESRoleMapping = $ADtoESMap[$ADGroup]
-  $userList = New-Object -TypeName System.Collections.ArrayList 
-  Get-ADGroupMember -Server $ADServer -Identity $ADGroup | ForEach-Object {
-    $userList.Add($_.SamAccountName)
-  }
-  WriteLog "Got users."
-  $jsonHash = (@{ "all" = @{ "field" = @{ "username" = $userList }}, @{"field" = @{ "realm.name" = "<auth_realm>" }} })
-  WriteLog "Getting $ESRoleMapping."
-  $body = ((( Invoke-WebRequest -Method Get -Uri $url/endpoint/$ESRoleMapping -Headers @{Authorization = "ApiKey $apiKey"}).Content | ConvertFrom-Json).$ESRoleMapping | ConvertTo-Json -Depth 10 -Compress)
-  $body = $body -replace '{"all":\[(.+)\]}', $jsonHash
-  WriteLog "Putting update to $ESRoleMapping."
-  $res = Invoke-WebRequest -Method Get -Uri $url/endpoint/$ESRoleMapping -Headers @{Authorization = "ApiKey $apiKey"} -Body $body -ContentType "application/json"
-  If ($res.StatusCode -eq 200) {
-    WriteLog "Put successful."
-  } else {
-    WriteLog "Put unsuccessful. Stopping."
-    Exit(1)
-  }
+    $ESRoleMapping = $ADtoESMap[$ADGroup]
+    $userList = New-Object -TypeName System.Collections.ArrayList 
+    Get-ADGroupMember -Server $ADServer -Identity $ADGroup | ForEach-Object {
+        $userList.Add($_.SamAccountName)
+    }
+    WriteLog "Got users."
+    $jsonHash = (@{ "all" = @{ "field" = @{ "username" = $userList }}, @{"field" = @{ "realm.name" = "<auth_realm>" }} })
+    WriteLog "Getting $ESRoleMapping."
+    $body = ((( Invoke-WebRequest -Method Get -Uri $url/endpoint/$ESRoleMapping -Headers @{Authorization = "ApiKey $apiKey"}).Content | ConvertFrom-Json).$ESRoleMapping | ConvertTo-Json -Depth 10 -Compress)
+    $body = $body -replace '{"all":\[(.+)\]}', $jsonHash
+    WriteLog "Putting update to $ESRoleMapping."
+    $res = Invoke-WebRequest -Method Get -Uri $url/endpoint/$ESRoleMapping -Headers @{Authorization = "ApiKey $apiKey"} -Body $body -ContentType "application/json"
+    If ($res.StatusCode -eq 200) {
+        WriteLog "Put successful."
+    } else {
+        WriteLog "Put unsuccessful. Stopping."
+        Exit(1)
+    }
 } 
